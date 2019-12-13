24c051d8a36d1f12ddfec4c4d73f91905f68a223
Lewis Liu <lewisl9029@users.noreply.github.com>
2019-12-13 12:51:26 -0800

    Update shared_child.js

--- app/component/shared_child.js
+++ app/component/shared_child.js
@@ -2,7 +2,7 @@ import * as vdom from 'app/lib/vdom.js'
 
 const shared_child = () =>
   vdom.element('div', {
-    children: 'edit this to test reload in app/component/shared_child.js',
+    children: 'edit to test reload in app/component/shared_child.js',
   })
 
 export { shared_child as component }
